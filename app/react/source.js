755754838a2d538a2a00fbd329d657b60b55dcc8
Elise <elisecode247@gmail.com>
2016-11-10 02:14:47 +0000

    set up gulp tasks

---
+++ app/react/source.js
@@ -0,0 +1,6 @@
+'use strict';
+var react = require('react');
+console.log('test12');
+
+
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,57 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync');
-var sass = require('gulp-sass');
-var mocha = require('gulp-mocha');
-var gutil = require('gulp-util');
-var watch = require('gulp-watch');
+require('babel-core/register');
+const babel = require('gulp-babel');
+const browserify = require('gulp-browserify');
+const browserSync = require('browser-sync');
+const concat = require('gulp-concat');
+const eslint = require('gulp-eslint');
+const gulp = require('gulp');
+const gutil = require('gulp-util');
+const mocha = require('gulp-mocha');
+const sass = require('gulp-sass');
+const uglify = require('gulp-uglify');
+const watch = require('gulp-watch');
 
-gulp.task('mocha', function() {
-    return gulp.src(['tests/*.js'], { read: false })
+gulp.task('mocha', () => {
+    return gulp.src(['test/*.js'], { read: false })
         .pipe(mocha({ reporter: 'list' }))
         .on('error', gutil.log);
 });
-gulp.task('sass', function() {
-    return gulp.src('./app/sass/**/*.scss')
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(gulp.dest('./public/css'))
+
+gulp.task('scripts', () => {
+    gulp.src(['app/react/*.js'])
+        .pipe(browserify())
+        .pipe(uglify())
+        .pipe(concat('dest.js'))
+        .pipe(gulp.dest('./public/scripts'));
 });
 
-gulp.task('browser-sync', function() {
+gulp.task('sass', () => {
+    return gulp.src('./app/sass/**/*.scss')
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('browser-sync', () => {
     browserSync({
         port: 8082
     });
 });
 
-gulp.task('default', ['browser-sync'], function() {
+gulp.task('lint-js', () => {
+  return gulp.src([
+    'app/react/*.js',
+    'app/common/*.js',
+    'app/controllers/*.js',
+    'app/models/*.js',
+    'app/routes/*.js',
+    'app/config/*.js'
+  ])
+    .pipe(eslint())
+    .pipe(eslint.format());
+});
 
+gulp.task('default', ['browser-sync'], () => {
     gulp.watch(['public/index.html'], browserSync.reload);
     gulp.watch(['app/sass/styles.scss'], ['sass', browserSync.reload]);
-
+    gulp.watch(['app/react/*.js'], ['scripts', browserSync.reload]);
 });
